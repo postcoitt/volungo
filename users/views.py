e83bb56c13492af20be33aafd4854b331e0cb91e
alinaytsk <alinayatsko10@gmail.com>
2026-02-19 18:11:54 +0200

    Users page

---
+++ users/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def profile_view(request, username):
+    context = {
+        'username': username,
+        'full_name': 'Олексій Мороз',
+        'age': 30,
+        'level': 12,
+        'xp': '3400/5000',
+        # Тимчасові посилання на фото (можете замінити на свої)
+        'avatar_url': 'https://i.imgur.com/8Km9tLL.png',
+        'org_logo': 'https://i.imgur.com/he9S6y8.png'
+    }
+    return render(request, 'users/profile.html', context)
