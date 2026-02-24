c6373150324ddf61bba12bffac1ecd450f08d6ee
Maksym_Kyba <admin@admins-MacBook-Pro.local>
2026-02-24 14:02:28 +0200

    Add logic to create appropriate path to profile

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Тепер адреса буде просто: 127.0.0.1:8000/Olesia/
-    path('<str:username>/', views.profile_view, name='profile'),
+    path('profile/<str:username>/', views.profile_view, name='profile'),
+    path('my-profile/', views.my_profile_redirect, name='my_profile'),
 ]
-
--- users/views.py
+++ users/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Profile
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
 
 def profile_view(request, username):
     # Шукаємо профіль у базі даних за ім'ям користувача
-    profile = get_object_or_404(Profile, user__username=username)
+    user = User.objects.get(username=username)
+    profile = user.profile
 
     context = {
         'full_name': profile.full_name,
@@ -13,3 +16,8 @@ def profile_view(request, username):
         'avatar_url': profile.avatar.url if profile.avatar else 'https://i.imgur.com/8Km9tLL.png',
     }
     return render(request, 'users/profile.html', context)
+
+
+@login_required
+def my_profile_redirect(request):
+    return redirect('profile', username=request.user.username)
