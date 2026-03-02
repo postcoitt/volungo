1e2a73458d67a6c4b567ecfebe29e5978e4e71c7
Maksym_Kyba <maksymkyba09@gmail.com>
2026-03-02 23:44:53 +0200

    Empty URL now leads to map page

--- volungo/mapapp/urls.py
+++ volungo/mapapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('my-button-action/', views.my_button_action, name='button_action'),
     path('show_filters/', views.filters_button_action, name='show_filters'),
+    path('', views.interactive_map, name='map'),
     path('map/', views.interactive_map, name='map'),
     path('map/create/', views.create_event, name='create_event'),
     path('map/event/<int:event_id>/', views.event_detail_view, name='event_detail'),
