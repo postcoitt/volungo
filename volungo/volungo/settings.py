fa226e6caffe74f8aa584adb7a0c25e6a4dc4baa
Danylo Ryshko <danyloryshko1@gmail.com>
2026-03-03 17:00:35 +0200

    mobile now works

--- volungo/volungo/settings.py
+++ volungo/volungo/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-nqmnq64*&=ff3zdrl1c#x9l@c8-^owq^rvw(&-yc$cmt9hg#ls
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['nontheatrically-hastate-serena.ngrok-free.dev', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://nontheatrically-hastate-serena.ngrok-free.dev']
 
 # Application definition
 
