0e4cca2e64c205bba3cda733a49cc6e62726db11
Sofia <sofia.gavronska@gmail.com>
2026-03-04 06:28:50 +0200

    fix for mjbile search yayay

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls')), # Твій підключений додаток
+    # path('', include('users.urls')), # Твій підключений додаток
 ]
 
 # Цей рядок дозволяє показувати картинки на сайті
